a16e0b12202b34ceef3427affe07649055028383
Brag2gr8 <58727101+Brag2gr8@users.noreply.github.com>
2023-01-05 16:24:06 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+import { Dog } from "/Dog.js";
+import { dogs } from "/data.js";
+
+let dogIndex = 0;
+let nowDog = new Dog(dogs[dogIndex]);
+
+const liked = () => {
+    nowDog.setStatus(true)
+    setBadge();
+    setTimeout(() => getNewDog(), 2000);
+}
+
+const rejected = () => {
+    nowDog.setStatus(false)
+    setBadge();
+    setTimeout(() => getNewDog(), 2000);
+}
+
+const getNewDog = () => {
+    dogIndex++;
+   if(dogIndex === 3) {dogIndex = 0}
+    nowDog = new Dog(dogs[dogIndex]);
+    render();
+    
+}
+
+const setBadge = () => document.getElementById("root").innerHTML += nowDog.getBadgeHtml();
+const render = () => document.getElementById("root").innerHTML = nowDog.getDogHtml();
+
+document.getElementById("heart").addEventListener("click", liked)
+document.getElementById("cross").addEventListener("click", rejected)
+
+render()
